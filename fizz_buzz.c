e3fc85aaba5ecaafbed2c129d86c935e2412eb87
unknown <idamakantibharath@gmail.com>
2025-02-12 10:24:05 +0530

    Prints the fizz buzz series

---
+++ fizz_buzz.c
@@ -0,0 +1,43 @@
+/*
+Write a program to print the series depending on the following condition
+
+1. if the value is multiplication of 3 - Fizz
+
+2. if the value is multiplication of 5 - Buzz
+
+3. if the value is multiplication of 3 and 5 - FizzBuzz
+
+Print the series from 1 to 100
+
+Output:
+1 2 Fizz 4 Buzz Fizz 7 8 Fizz Buzz 11 Fizz 13 14 FizzBuzz 16 17 Fizz 19 Buzz
+ Fizz 22 23 Fizz Buzz 26 Fizz 28 29 FizzBuzz 31 32 Fizz 34 Buzz Fizz 37 38 Fizz Buzz
+  41 Fizz 43 44 FizzBuzz 46 47 Fizz 49 Buzz Fizz 52 53 Fizz Buzz 56 Fizz 58 59 FizzBuzz
+   61 62 Fizz 64 Buzz Fizz 67 68 Fizz Buzz 71 Fizz 73 74 FizzBuzz 76 77 Fizz 79 Buzz
+    Fizz 82 83 Fizz Buzz 86 Fizz 88 89 FizzBuzz 91 92 Fizz 94 Buzz Fizz 97 98 Fizz Buzz
+*/
+#include<stdio.h>
+int main()
+{
+    for(int i = 1; i<=100; i++)
+    {
+        if((i%3==0) && (i%5==0))
+        {
+            printf("FizzBuzz ");
+        }
+        else if(i%3 == 0)
+        {
+            printf("Fizz ");
+        }
+        else if(i%5 == 0)
+        {
+            printf("Buzz ");
+        }
+        else
+        {
+            printf("%d ",i);
+        }
+        
+    }
+    return 0;
+}

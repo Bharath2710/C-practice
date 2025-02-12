56792ea2c692640d4702c47cc82403a7c57ea02e
unknown <idamakantibharath@gmail.com>
2025-02-12 19:30:50 +0530

    Program to Print Simple Triangle Digits Pattern

---
+++ simple_triangle_digits.c
@@ -0,0 +1,19 @@
+/*
+Program to Print Simple Triangle Digits Pattern
+*/
+#include<stdio.h>
+int main()
+{
+    int num;
+    scanf("%d", &num);
+    
+    for(int i = 1; i <= num; i++)
+    {
+        for(int j = 1; j <= i; j++)
+        {
+            printf("%d ", j);
+        }
+        printf("\n");
+    }
+    return 0;
+}
